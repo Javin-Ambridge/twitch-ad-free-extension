3dcb7ac0d58a0d715d81ba74442daaf903001b8e
Javin <javin.ambridge@gmail.com>
2021-06-24 20:32:37 -0700

    removing background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-console.log("Now here!");
-
-// Listener for newly loaded pages
-function onMsgListener(request, sender) {
-  console.log("HERE: ", request);
-}
-
-// Register listener
-chrome.runtime.onMessage.addListener(onMsgListener);
